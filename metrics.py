b12993a92541480507092b70505fdb4ca649a092
siragerkol <siragerkol@gmail.com>
2022-10-10 21:16:10 -0400

    citation moment

--- metrics.py
+++ metrics.py
@@ -4,7 +4,11 @@ def E_index(c): # c: a list of citations received for each publication
 	N = len(c)
 	c = np.array(c[c!=0])
 	q = c / sum(c)
-
 	E = -sum(c * np.log(q)) / N 
 	return (E)
 
+
+def citation_moment(c, alpha):
+	c = np.array(c)
+	M_alpha = np.mean(c**alpha)
+	return(M_alpha)
