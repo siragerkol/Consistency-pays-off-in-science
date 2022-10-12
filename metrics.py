ac9d0f22fca27f04d26b3cdb4165626f9351ded3
siragerkol <siragerkol@gmail.com>
2022-10-12 15:25:38 -0400

    h-index function

--- metrics.py
+++ metrics.py
@@ -1,25 +1,40 @@
 import numpy as np
 
+'''
+c: list of citations received for each publication
+'''
+
 def E_index(c):
 	'''
 	function for calculating E-index
-	c: list of citations received for each publication
 	'''
 	c = np.array(c)
 	N = len(c)
 	c = c[c!=0]
 	q = c / sum(c)
 	E = -sum(c * np.log(q)) / N 
-	return (E)
+
+	return(E)
 
 
 def citation_moment(c, alpha=0.3):
 	'''
 	function for calculating citation moment (M_alpha)
-	c: list of citations received for each publication
 	alpha: parameter for modulating the weight of citation distribution, should be alpha >= 0
 	'''
 	c = np.array(c)
 	M_alpha = np.mean(c**alpha)
+
 	return(M_alpha)
 
+
+def h_index(c):
+	'''
+	function for calculating h-index
+	'''
+    c = np.array(c)
+    paper_ids = np.arange(1, len(c)+1)
+    c[::-1].sort()
+    H = np.max(np.minimum(c, paper_ids))
+
+    return(H)
