02f7477ea37d14c1c63e1da57a0126f3b5e83f65
siragerkol <siragerkol@gmail.com>
2022-10-12 15:15:57 -0400

    e-index fix

--- metrics.py
+++ metrics.py
@@ -5,8 +5,9 @@ def E_index(c):
 	function for calculating E-index
 	c: list of citations received for each publication
 	'''
+	c = np.array(c)
 	N = len(c)
-	c = np.array(c[c!=0])
+	c = c[c!=0]
 	q = c / sum(c)
 	E = -sum(c * np.log(q)) / N 
 	return (E)
@@ -21,3 +22,4 @@ def citation_moment(c, alpha=0.3):
 	c = np.array(c)
 	M_alpha = np.mean(c**alpha)
 	return(M_alpha)
+
